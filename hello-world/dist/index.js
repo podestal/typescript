8a034937c4ad8853992fe62fc67d06aa541a53c4
podestal <luisrodriguezpodesta@gmail.com>
2024-03-03 14:36:02 -0500

    basic types

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,2 +1,27 @@
 "use strict";
-let age = 20;
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+let numbers = [];
+let user = [1, 'luis'];
+let mySize = 2;
+console.log(mySize);
+const calculateTaxVoid = (income) => {
+    console.log('This fnc does not return anything', income);
+};
+const calculateTax = (income, taxYear = 2022) => {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+};
+console.log('Tax calculation without year: $.', calculateTax(10000));
+console.log('Tax calculation with year: $.', calculateTax(10000, 2020));
+let employee = {
+    id: 1,
+    name: "Luis",
+    retire: (date) => console.log(date)
+};
+console.log('Employee', employee);
+//# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,5 +1,54 @@
-let age: number = 20;
-if (age < 50) {
-    age += 10
+// BUILT IN TYPES
+
+let sales: number = 123_456_789;
+let course: string = 'TypeScript'
+let is_published: boolean = true
+let level: any
+
+// ARRAYS
+
+// let numbers: number[] = [1, 2, 4]
+let numbers: number[] = []
+
+// TUPLES: fixed length array
+let user: [number, string] = [1, 'luis']
+
+// ENUMS
+ const enum Size { Small = 1, Medium = 2, Large = 3 }
+ let mySize: Size = Size.Medium
+ console.log(mySize);
+
+//  FUNCTIONS
+
+const calculateTaxVoid = ( income: number): void => {
+    console.log('This fnc does not return anything', income);
+    
 }
 
+const calculateTax = (income: number, taxYear= 2022): number => {
+    if (taxYear < 2022) {
+        return income * 1.2
+    }
+    return income * 1.3
+}
+
+console.log('Tax calculation without year: $.', calculateTax(10_000))
+console.log('Tax calculation with year: $.', calculateTax(10_000, 2020))
+
+// OBJECTS
+
+let employee :{ 
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1, 
+    name: "Luis",
+    retire: (date: Date) => console.log(date)
+}
+
+console.log('Employee', employee, );
+
+
+
+ 
